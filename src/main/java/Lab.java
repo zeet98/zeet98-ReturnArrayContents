66e8e1283764fbaea65e55103a3728bee7302e79
Zeshan Tawa <zeshan.tawa98@gmail.com>
2023-02-23 21:14:23 +0000

    Complete ReturnArray assignment

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -30,6 +30,10 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        String str = "";
+        for(int i : nums){
+            str = str + i + " ";
+        }
+        return str;
     }
 }
